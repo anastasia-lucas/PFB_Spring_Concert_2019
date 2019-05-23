b76aac6e3b1ca5ea1d5f366cb74b76fb045a9bfe
Anastasia Lucas <36303235+anastasia-lucas@users.noreply.github.com>
2019-05-23 09:52:25 -0400

    Update launch_twelve_seconds_to_the_moon.py

--- launch_twelve_seconds_to_the_moon.py
+++ launch_twelve_seconds_to_the_moon.py
@@ -24,9 +24,9 @@ for line in fr.xreadlines():
 init()
 
 print("\n")
-print(" ------------------------------------------------")
+print("+------------------------------------------------+")
 print("| SONG                               |    STATUS |")
-print(" ------------------------------------------------")
+print("+------------------------------------------------+")
 print("| Also Sprach Zarathustra            | COMPLETED |")
 print("| Selections from ET                 | COMPLETED |")
 print("| We Seven                           | COMPLETED |")
@@ -36,9 +36,9 @@ print("| Twelve Seconds to the Moon         |       RUN |")
 print("| Doctor Who: Through Space and Time |   PENDING |")
 print("| Through the Rings of Saturn        |   PENDING |")
 print("| Life on Mars                       |   PENDING |")
-print(" ------------------------------------------------")
+print("+------------------------------------------------+")
 print("| CONCERT PROGRESS                   |       55% |")
-print(" ------------------------------------------------\n")
+print("+------------------------------------------------+\n")
 
 def tprint(words):
     for char in words:
