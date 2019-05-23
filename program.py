012985b931dfc08f7d870d489d021e51a5d6d04e
Anastasia Lucas <36303235+anastasia-lucas@users.noreply.github.com>
2019-05-23 09:43:49 -0400

    Create program.py

---
+++ program.py
@@ -0,0 +1,30 @@
+load("PFB_in_Space/Program")
+
+print("Welcome to our concert!")
+
+get_song <- function(title, composer){
+	song <- paste(title, composer, sep=" by ")
+	return(song)
+}
+
+get_song("Also Sprach Zarathustra", "Richard Strauss/ arr. Longfield")
+get_song("Selections from ET", "John Williams/ arr. Cacavas")
+get_song("We Seven", "Derek M. Jenkins")
+get_song("Fly Me to the Moon", "Bart Howard/ arr. Frabizio")
+get_song("Star Trek Into Darkness", "Arr. Michael Brown")
+
+Intermission <- function(x){
+	break
+}
+
+get_song("Twelve Seconds to the Moon", "Robert W. Smith")
+get_song("Doctor Who: Through Space and Time", "Murray Gold/ arr. Buckley")
+get_song("Through the Rings of Saturn", "Ben Kirby")
+get_song("Life on Mars", "David Bowie/ arr. Bernaerts")
+
+i <- 3
+while(i > 0){
+	print(i)
+	i <- i - 1
+}
+print("Blast off!")
