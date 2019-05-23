f862d129f47a4c6bdcc4ea649d39741beb48173e
Anastasia Lucas <36303235+anastasia-lucas@users.noreply.github.com>
2019-05-23 09:46:27 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,11 +1,10 @@
-load("PFB_in_Space/Program")
+#from PFB_in_Space import Program 
 
 print("Welcome to our concert!")
 
-get_song <- function(title, composer){
-	song <- paste(title, composer, sep=" by ")
-	return(song)
-}
+def get_song(title, composer):
+	print(title + " by " + composer + "\n")
+	return;
 
 get_song("Also Sprach Zarathustra", "Richard Strauss/ arr. Longfield")
 get_song("Selections from ET", "John Williams/ arr. Cacavas")
@@ -13,18 +12,17 @@ get_song("We Seven", "Derek M. Jenkins")
 get_song("Fly Me to the Moon", "Bart Howard/ arr. Frabizio")
 get_song("Star Trek Into Darkness", "Arr. Michael Brown")
 
-Intermission <- function(x){
-	break
-}
+def Intermission(x=15):
+	while x<15:
+		break
 
 get_song("Twelve Seconds to the Moon", "Robert W. Smith")
 get_song("Doctor Who: Through Space and Time", "Murray Gold/ arr. Buckley")
 get_song("Through the Rings of Saturn", "Ben Kirby")
 get_song("Life on Mars", "David Bowie/ arr. Bernaerts")
 
-i <- 3
-while(i > 0){
-	print(i)
-	i <- i - 1
-}
-print("Blast off!")
+i = 3
+while i > 0:
+	print i
+	i = i - 1
+print "Blast off!"
